f737d0e1c085e5857807046e097ceb4f9c046945
Martijn Brekhof <martijn@atcomputing.nl>
2015-11-12 12:57:36 +0100

    Implemented creating empty music video files

--- Makefile
+++ Makefile
@@ -12,5 +12,9 @@ movies:
 	mkdir -p media/movies
 	cd media/movies && gunzip -c ../../data/movies.list.gz | ../../createmovies.pl 1000
 
+musicvideos:
+	mkdir -p media/musicvideos
+	cd media/musicvideos && cat ../../data/musicvideos.list | ../../createmusicvideos.pl
+
 clean:
 	rm -rf media
---
+++ createmusicvideos.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+
+# Copyright 2015 Martijn Brekhof
+#
+# This file is part of dummymediafiles.
+#
+# dummymediafiles is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# dummymediafiles is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with dummymediafiles.  If not, see <http://www.gnu.org/licenses/>.
+
+use strict;
+use warnings;
+
+while(<>) {
+	chomp;
+	my $file = $_ . ".mp4";
+	if ( open(FW,">${file}") ) {
+			print FW "";
+			close FW;
+	}
+}
